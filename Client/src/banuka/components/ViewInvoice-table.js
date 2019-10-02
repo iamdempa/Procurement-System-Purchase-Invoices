4be2535ff7c9eedf5e9b1ba0dcd65d30d5411fd1
iamdempa <jbjayarathna@gmail.com>
2019-10-03 00:10:18 +0530

    only view one invoice

--- Client/src/banuka/components/ViewInvoice-table.js
+++ Client/src/banuka/components/ViewInvoice-table.js
@@ -1,8 +1,146 @@
 import React, { Component } from "react";
 import { MDBTable, MDBTableBody, MDBTableHead } from "mdbreact";
+import {Link} from 'react-router-dom';
 
 
+const styles = {
+  mouse: {
+    cursor: "pointer"
+  }
+};
+export default class ParentComponent extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      data: [
+        {
+          id: 1,
+          date: "2014-04-18",
+          total: 121.0,
+          status: "Shipped",
+          name: "A",
+          points: 5,
+          percent: 50
+        },
+        {
+          id: 2,
+          date: "2014-04-21",
+          total: 121.0,
+          status: "Not Shipped",
+          name: "B",
+          points: 10,
+          percent: 60
+        },
+        {
+          id: 3,
+          date: "2014-08-09",
+          total: 121.0,
+          status: "Not Shipped",
+          name: "C",
+          points: 15,
+          percent: 70
+        },
+        {
+          id: 4,
+          date: "2014-04-24",
+          total: 121.0,
+          status: "Shipped",
+          name: "D",
+          points: 20,
+          percent: 80
+        },
+        {
+          id: 5,
+          date: "2014-04-26",
+          total: 121.0,
+          status: "Shipped",
+          name: "E",
+          points: 25,
+          percent: 90
+        }
+      ],
+      expandedRows: []
+    };
+  }
+
+  handleRowClick(rowId) {
+    const currentExpandedRows = this.state.expandedRows;
+    const isRowCurrentlyExpanded = currentExpandedRows.includes(rowId);
+
+    const newExpandedRows = isRowCurrentlyExpanded
+      ? currentExpandedRows.filter(id => id !== rowId)
+      : currentExpandedRows.concat(rowId);
+
+    this.setState({ expandedRows: newExpandedRows });
+  }
+
+  renderItem(item) {
+    const clickCallback = () => this.handleRowClick(item.id);
+    const itemRows = [
+      <tr onClick={clickCallback} key={"row-data-" + item.id}>
+        <td>{item.date}</td>
+        <td>{item.total}</td>
+        <td>{item.status}</td>
+        <td>{item.date}</td>
+        <td>{item.total}</td>
+        <td>{item.status}</td>
+        <Link to="/banuka/view/:id"><td><i className="fa fa-pen"></i></td></Link>
+      </tr>
+    ];
+
+    if (this.state.expandedRows.includes(item.id)) {
+      itemRows.push(
+
+        <tr key={"row-expanded-" + item.id} className="alert alert-primary">
+          <td>{item.name}</td>
+          <td>{item.points}</td>
+          <td>{item.percent}</td>
+          <td>{item.name}</td>
+          <td>{item.points}</td>
+          <td>{item.percent}</td>
+        </tr>
+
+       
+      );
+    }
+
+    return itemRows;
+  }
+
+  render() {
+    let allItemRows = [];
+
+    this.state.data.forEach(item => {
+      const perItemRows = this.renderItem(item);
+      allItemRows = allItemRows.concat(perItemRows);
+    });
+
+    return (
+      <MDBTable
+        className="container-fluid"
+        hover
+        responsive
+        style={styles.mouse}
+      >
+        <MDBTableHead color="primary-color" textWhite>
+          <tr>
+            <th>Invoice No</th>
+            <th>Vendor Name</th>
+            <th>Invoice Date</th>
+            <th>Expected Delivery</th>
+            <th>Billing Address</th>
+            <th>Contact Person</th>
+            <th></th>
+          </tr>
+        </MDBTableHead>
+        <MDBTableBody>{allItemRows}</MDBTableBody>
+      </MDBTable>
+    );
+  }
+}
+
+/*
 export default class ViewInvoice extends Component {
   render() {
     return (
@@ -54,3 +192,4 @@ export default class ViewInvoice extends Component {
     );
   }
 }
+*/
